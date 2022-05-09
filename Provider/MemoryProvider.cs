81f78453395caebf3688306dbdf12484970b7d9c
Thorsten <zkoddable@gmail.com>
2022-05-09 11:39:28 +0200

    Added Memory Provider

---
+++ Provider/MemoryProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace H2HY.Provider
+{
+    public class MemoryProvider<T> : IProvider<T>
+    {
+        private HashSet<T> _memoryList = new HashSet<T>();
+
+        public void Add(T item)
+        {
+            _memoryList.Add(item);
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            _memoryList.UnionWith(items);
+        }
+
+        public void Clear()
+        {
+            _memoryList.Clear();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _memoryList;
+        }
+
+        public bool Remove(T item)
+        {
+            return _memoryList.Remove(item);
+        }
+
+        public void SaveAll(IEnumerable<T> items)
+        {
+            //NOP
+        }
+
+        public bool Update(T item)
+        {
+            //NOP
+            return true;
+        }
+    }
+}
