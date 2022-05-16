3768aa413f5c17657a7c3ebaa56e111a79c2dc96
Thorsten <zkoddable@gmail.com>
2022-05-16 08:58:22 +0200

    added NavigationCommand

--- Services/DialogService.cs
+++ Services/DialogService.cs
@@ -1,5 +1,4 @@
-﻿using H2HY;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows;
 
---
+++ ToolKit/NavigateCommand.cs
@@ -0,0 +1,19 @@
+﻿using H2HY.Services;
+
+namespace H2HY.Toolkit
+{
+    public class NavigateCommand<TViewModel> : CommandBase where TViewModel : ViewModelBase
+    {
+        private readonly NavigationService<TViewModel> _navigationService;
+
+        public NavigateCommand(NavigationService<TViewModel> navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationService.Navigate();
+        }
+    }
+}
