50fc427113053387399422636b69fcb8f0c87d84
Thorsten <zkoddable@gmail.com>
2022-12-07 13:04:29 +0100

    code cleaning

--- Stores/StoreEventArgs.cs
+++ Stores/StoreEventArgs.cs
@@ -3,6 +3,22 @@
 namespace H2HY.Stores
 {
     /// <summary>
+    /// Indicates a perfomed action on an sinlge item.
+    /// </summary>
+    public enum NotifyStoreChangedAction
+    {
+        /// <summary>
+        /// An item was added to the collection.
+        /// </summary>
+        Add = 0,
+
+        /// <summary>
+        /// An item was removed from the collection.
+        /// </summary>
+        Remove = 1,
+    }
+
+    /// <summary>
     /// Generic Args for store-changed action.
     /// </summary>
     /// <typeparam name="T"></typeparam>
