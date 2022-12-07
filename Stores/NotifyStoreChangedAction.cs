179f0a71b99ce6d213fe610fbe170d34b59a86e5
Thorsten <zkoddable@gmail.com>
2022-12-07 12:55:47 +0100

    Added generic StoreEventArgs

---
+++ Stores/NotifyStoreChangedAction.cs
@@ -0,0 +1,18 @@
+﻿namespace H2HY.Stores
+{
+    /// <summary>
+    /// Indicates a perfomed action on an sinlge item.
+    /// </summary>
+    public enum NotifyStoreChangedAction
+    {
+        /// <summary>
+        /// An item was added to the collection.
+        /// </summary>
+        Add = 0,
+
+        /// <summary>
+        /// An item was removed from the collection.
+        /// </summary>
+        Remove = 1,
+    }
+}
---
+++ Stores/StoreEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace H2HY.Stores
+{
+    /// <summary>
+    /// Generic Args for store-changed action.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class StoreEventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// Affected Value
+        /// </summary>
+        public T Value { get; private set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="val">affected value</param>
+        /// <param name="action">performed action</param>
+        public StoreEventArgs(T val, NotifyStoreChangedAction action)
+        {
+            Value = val;
+            Action = action;
+        }
+
+        /// <summary>
+        /// performed action
+        /// </summary>
+        public NotifyStoreChangedAction Action { get; }
+    }
+}
