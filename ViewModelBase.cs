12f57024b342e33e6c8ce0affc953fa2e59b190d
Thorsten <zkodd@proton.me>
2023-07-25 18:42:38 +0200

    added AttributeState

--- ViewModelBase.cs
+++ ViewModelBase.cs
@@ -1,85 +1,40 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿
+namespace H2HY;
 
-namespace H2HY
+/// <summary>
+/// Base class for all view models.
+/// </summary>
+public abstract class ViewModelBase : NotifyPropertyChanged
 {
     /// <summary>
-    /// Base class for all view models. Realises INotifyPropertyChanged.
+    /// abstract forces every derivation to implement a dispose function to avoid memory leaks.
     /// </summary>
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public abstract void Dispose();
+
+    /// <summary>
+    /// The attached view has been closed without a result. (modal or nonmodal)
+    /// </summary>
+    public virtual void ViewClosed()
     {
-        /// <summary>
-        /// Property Changed event.
-        /// </summary>
-        public event PropertyChangedEventHandler? PropertyChanged;
+    }
 
-        /// <summary>
-        /// abstract forces every derivation to implement a dispose function to avoid memory leaks.
-        /// </summary>
-        public abstract void Dispose();
+    /// <summary>
+    /// The attached modal view has been closed with the given result.
+    /// </summary>
+    /// <param name="dialogResult"></param>
+    public virtual void ViewClosed(bool dialogResult)
+    {
+    }
 
-        /// <summary>
-        /// The attached view has been closed without a result. (modal or nonmodal)
-        /// </summary>
-        public virtual void ViewClosed()
-        {
-        }
+    internal void DisposeAll()
+    {
+        // this is not possible "out of a package"
+        //foreach (PropertyInfo propertyInfo in GetType().GetProperties().Where(p => p.PropertyType.IsSubclassOf(typeof(ViewModelBase))))
+        //{
+        //    MethodInfo m = propertyInfo.PropertyType.GetMethod(nameof(Dispose), new Type[0] { });
+        //    _ = m.Invoke(propertyInfo.GetValue(this), new object[] { });
+        //}
 
-        /// <summary>
-        /// The attached modal view has been closed with the given result.
-        /// </summary>
-        /// <param name="dialogResult"></param>
-        public virtual void ViewClosed(bool dialogResult)
-        {
-        }
-
-        internal void DisposeAll()
-        {
-            // this is not possible "out of a package"
-            //foreach (PropertyInfo propertyInfo in GetType().GetProperties().Where(p => p.PropertyType.IsSubclassOf(typeof(ViewModelBase))))
-            //{
-            //    MethodInfo m = propertyInfo.PropertyType.GetMethod(nameof(Dispose), new Type[0] { });
-            //    _ = m.Invoke(propertyInfo.GetValue(this), new object[] { });
-            //}
-
-            Dispose();
-        }
-
-        /// <summary>
-        /// Calls property Changed for the given Propertyname. Default is CallerMembername.
-        /// </summary>
-        /// <param name="propertyName">[CallerMemberName]</param>
-        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// Raises property changed for all properties.
-        /// </summary>
-        protected void RaisePropertyChangedAll()
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(string.Empty));
-        }
-
-        /// <summary>
-        /// Sets a field and raises PropertyChanged in case of an change.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="fieldName">ref to field</param>
-        /// <param name="newValue">New value</param>
-        /// <param name="propertyName">Default [CallerMemberName]</param>
-        protected bool SetProperty<T>(ref T fieldName, T newValue, [CallerMemberName] string propertyName = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(fieldName, newValue))
-            {
-                return false;
-            }
-
-            fieldName = newValue;
-            RaisePropertyChanged(propertyName);
-            return true;
-        }
+        Dispose();
     }
 }
